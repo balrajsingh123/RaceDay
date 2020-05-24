04d1b867373b2ab9aa2de9f3aca0f0542ed19d8d
Balraj <balrajgill236@gmail.com>
2020-05-24 13:21:36 +1200

    Add project files.

---
+++ RaceDay/Suman.cs
@@ -0,0 +1,12 @@
+﻿namespace RaceDay
+{
+    class Suman : Punter // Suman class inherting the feature of Punter class
+    {
+        public Suman() // Suman Constructor
+        {
+            PunterName = "Suman"; // Punter name
+            CashOnHand = 50; // Initial amount for punter
+            Alias = "Suman"; // Punter Nick Name
+        }
+    }
+}
